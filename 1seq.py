fb3401631b1d52806e414ec33a1be6e600973900
Mihail_AI <m89531276499@gmail.com>
2022-12-05 18:24:43 +0300

    2 вопрос

---
+++ 1seq.py
@@ -0,0 +1,8 @@
+count = int(input('Введите количество элементов: '))
+lst = []
+
+for i in range(count):
+    arg = int(input(f'Введите {i+1} элемент: '))
+    lst.append(arg)
+
+print("Вывод:", lst)
