f79f4c49f1e3cc3db7f7979be17a3131f71121cb
Mihail_AI <m89531276499@gmail.com>
2022-12-05 19:12:42 +0300

    3 вопрос (с дополнением)

--- 2seq.py
+++ 2seq.py
@@ -1,5 +1,18 @@
-user = input('Введите элементы списка через запятую: ').split(',')
-user = list(map(int, user))
-user = list(set(user))
+separator = input('Выберете разделитель: , ; / (но только один!) \n ')
 
-print('Результат: ', user)
+if len(separator) > 1:
+    print("выберите одно значение!")
+    separator = input('Выберете разделитель: , ; / (но только один!) \n ')
+
+s = [',', '/', ';']
+while separator not in s:
+    separator = input('Выберите разделитель из предложеныых  , ; / \n')
+
+try:
+    user = input('Вводите элементы через выбранный разделитель: ').split(separator)
+    user = list(map(int, user))
+    user = list(set(user))
+
+    print('Результат: ', user)
+except:
+    print('Повнимательней!')
